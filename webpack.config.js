10e70c9154446f23ea8699c5239ea6aa3c6f0c22
YiJiong <yijiongwtt@outlook.com>
2018-01-17 21:20:26 +0800

    'layout_ok'

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var packCSS = new ExtractTextPlugin('./css/[name].min.css');
+require('./dist/css/bootstrap.min.css');
+
 module.exports = {
 	entry: {
 
@@ -5,5 +9,20 @@ module.exports = {
 	output: {
 		filename: '[name].bundle.js',
 		path: __dirname + '/dist/js'
-	}
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loader: ['style', 'css'],
+				include: APP_PATH
+			}
+		]
+	},
+	postcss: function () {
+		return [autoprefixer, cssnext, precss, cssnano]
+	},
+	plugins: [
+		packCSS
+	]
 }
